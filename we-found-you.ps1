252a284be8903a222fc5aaa6bdbc62a33e15f34c
HonoEagle <79271536+HonoEagle@users.noreply.github.com>
2023-01-18 01:05:46 +0100

    B
    a

--- we-found-you.ps1
+++ we-found-you.ps1
@@ -1,23 +1,62 @@
 $publicIP = (Invoke-WebRequest -Uri "http://checkip.dyndns.org" -UseBasicParsing).Content.Split(":")[1].Trim().Split("<")[0]
 $username = $env:username
 $os = (Get-WmiObject -Class Win32_OperatingSystem).Name
-$wifi = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq $True}).Description
-$ssid = (netsh wlan show interface | Select-String "SSID").Split(":")[1].Trim()
-$wifiPassword = (netsh wlan show profileparam name=$ssid | Select-String "Key Content").Split(":")[1].Trim()
+#$wifi = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq $True}).Description
 
-<#
-$s=New-Object -ComObject SAPI.SpVoice
-$s.Rate = -2
-$s.Speak("We found you $FN")
-$s.Speak("We know where you are")
-$s.Speak("We are everywhere")
-$s.Speak("We do not forgive, we do not forget")
-$s.Speak("Expect us")
-#>
+$networkAdapter = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq $True}
 
-Write-Host "Public IP: $publicIP"
-Write-Host "Username: $username"
-Write-Host "Operating System: $os"
-Write-Host "Wifi Adapter: $wifi"
-Write-Host "SSID: $ssid"
-Write-Host "Wifi Password: $wifiPassword"
+if ($networkAdapter.DHCPEnabled -eq $True) {
+    $ipType = "DHCP"
+} else {
+    $ipType = "Static"
+}
+
+$networkName = $networkAdapter.Description
+$ipAddress = $networkAdapter.IPAddress[0]
+$subnetMask = $networkAdapter.IPSubnet[0]
+$defaultGateway = $networkAdapter.DefaultIPGateway[0]
+
+
+#For powershell
+Write-Host "Network Adapter: $networkName"
+Write-Host "IP Type: $ipType"
+Write-Host "IP Address: $ipAddress"
+Write-Host "Subnet Mask: $subnetMask"
+Write-Host "Default Gateway: $defaultGateway"
+
+
+
+# Write information to text file
+$text = "We know where you are $publicIP
+We know who you are $username
+We know on what you work $os
+                        ____________
+                      .~      ,   . ~.
+                     /                \
+                    /      /~\/~\   ,  \
+                   |   .   \    /   '   |
+                   |         \/         |
+          XX       |  /~~\        /~~\  |       XX
+        XX  X      | |  o  \    /  o  | |      X  XX
+      XX     X     |  \____/    \____/  |     X     XX
+ XXXXX     XX      \         /\        ,/      XX     XXXXX
+X        XX%;;@      \      /  \     ,/      @%%;XX        X
+X       X  @%%;;@     |           '  |     @%%;;@  X       X
+X      X     @%%;;@   |. ` ; ; ; ;  ,|   @%%;;@     X      X
+ X    X        @%%;;@                  @%%;;@        X    X
+  X   X          @%%;;@              @%%;;@          X   X
+   X  X            @%%;;@          @%%;;@            X  X
+    XX X             @%%;;@      @%%;;@             X XX
+      XXX              @%%;;@  @%%;;@              XXX
+                         @%%;;%%;;@
+                           @%%;;@
+                         @%%;;@..@@
+                          @@@  @@@
+
+
+
+"
+$text | Out-File -FilePath "C:\info.txt" -Encoding UTF8
+
+# Open the text file
+Start-Process "notepad.exe" "C:\info.txt"
